843e188d7111ccca05efa4782a5fec1a03a108cc
Slik1Slik <Slik1Slik@users.github.com>
2021-06-28 17:21:49 +0300

    Initial Commit

---
+++ TwoDaysOnTwoDaysOff/TwoDaysOnTwoDaysOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoDaysOnTwoDaysOffApp.swift
+//  TwoDaysOnTwoDaysOff
+//
+//  Created by Slik on 28.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TwoDaysOnTwoDaysOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
